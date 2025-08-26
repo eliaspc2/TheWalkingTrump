d99c0a5d60285c6b607990f7630b8c9f781c7c0c
eliaspc2 <eliaspc@yahoo.com>
2025-08-26 09:09:25 +0100

    Rename Trump to Mr. T

--- configs.py
+++ configs.py
@@ -1,17 +1,24 @@
 import pygame
 
+# Inicializa os módulos principais do pygame
 pygame.init()
 pygame.mixer.init()
 
+
+# Configurações básicas da janela do jogo
 class window:
     WIDTH = 946
     HEIGHT = 549
-    TITLE = "The Walking Trump"
+    TITLE = "The Walking Mr. T"
     MARGIN = 30
 
+
+# Espaço reservado para definição de fontes (não utilizado)
 class font:
     ...
 
+
+# Carrega e configura os sons utilizados no jogo
 class sound:
     BACKGROUND = pygame.mixer.Sound("sounds/background.mp3")
     BACKGROUND.set_volume(0.50)
@@ -28,12 +35,14 @@ class sound:
     GAMEOVER = pygame.mixer.Sound("sounds/gameover.mp3")
     GAMEOVER.set_volume(0.50)
 
+
+# Carrega as imagens utilizadas no jogo
 class skin:
     BACKGROUND = pygame.image.load("imgs/background.png")
     CAR = pygame.image.load("imgs/car.png")
     LIFE = pygame.image.load("imgs/life.png")
     MISSIL = pygame.image.load("imgs/missil.png")
     POOP = pygame.image.load("imgs/poop.png")
-    TRUMP = pygame.image.load("imgs/trump.png")
+    MR_T = pygame.image.load("imgs/mr_t.png")
     GAMEOVER = pygame.image.load("imgs/gameover.png")
     GAMEWIN = pygame.image.load("imgs/gamewin.png")
